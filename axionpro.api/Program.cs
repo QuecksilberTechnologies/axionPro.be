662215fef0bb8f5598819b4043c4d2efbabeccb3
deepesh gupta <qtechnologies09@outlook.com>
2026-03-21 08:34:36 +0530

    ok

--- axionpro.api/Program.cs
+++ axionpro.api/Program.cs
@@ -148,8 +148,8 @@ try
     app.MapControllers();
     // check
   // //  // ✅ Dynamic port (for hosting like Heroku)w
-//     var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
- // app.Urls.Add($"http://*:{port}");
+     var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+   app.Urls.Add($"http://*:{port}");
    
    await app.RunAsync();
     
